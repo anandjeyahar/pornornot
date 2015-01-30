8d5263ac77d5167ea9cd57007feb22555068b715
Software Mechanic <softwaremechanic32@gmail.com>
2015-01-31 00:36:15 +0530

    cleaning up and starting from fresh

--- mobile/pornOrNot/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/CordovaLib/AndroidManifest.xml \
--- mobile/pornOrNot/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/ant-build/AndroidManifest.xml : \
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/AndroidManifest.xml \

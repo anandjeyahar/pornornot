ad463761d85ea4e527b68140920411ae726a4135
Anand Jeyahar <anand.jeyahar@gmail.com>
2015-01-31 00:36:15 +0530

    cleaning up and starting from fresh

--- mobile/pornOrNot/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/ant-build/CordovaApp.ap_ \
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/ant-build/classes.dex \
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/CordovaLib/ant-build/classes.jar \
-/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/libs/google-volley.jar \

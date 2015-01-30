86d217634161088d15da7db5a59628d730fd79e9
Anand Jeyahar <anand.jeyahar@gmail.com>
2015-01-31 00:11:36 +0530

    more changes

---
+++ mobile/pornOrNot/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ mobile/pornOrNot/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/ant-build/AndroidManifest.xml : \
+/home/anand/workspace/bitbucket/private/porndetect/mobile/pornOrNot/platforms/android/AndroidManifest.xml \

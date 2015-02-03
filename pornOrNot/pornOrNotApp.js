a1add87663e11d0ecf8892be539e33225f3ef25e
Software Mechanic <softwaremechanic32@gmail.com>
2015-02-03 09:36:42 +0530

    adding more redis keys

--- pornOrNot/pornOrNotApp.js
+++ pornOrNot/pornOrNotApp.js
@@ -33,24 +33,35 @@ if (settings.redis.password) {
 var processImgData = function(imgArgs) {
     var urlParts = imgArgs.imgUrl.split('/');
     var imgId = urlParts[urlParts.length - 1];
+    var errFunc = function (err, resp) {
+            if (err) throw err;
+            console.log('added ' + resp + 'items');
+        };
+
     switch (imgArgs.pornCategory) {
         case 'pussy':
-            client.sadd(P_SET, imgId);
+            client.sadd(P_SET, imgId, errFunc);
+            client.zadd(P_SET, imgId, 1, errFunc);
             break;
         case 'ass':
-            client.sadd(A_SET, imgId);
+            client.sadd(A_SET, imgId, errFunc);
+            client.zadd(A_SET, imgId, 1, errFunc);
             break;
         case 'dick':
-            client.sadd(D_SET, imgId);
+            client.sadd(D_SET, imgId, errFunc);
+            client.zadd(D_SET, imgId, 1, errFunc);
             break;
         case 'sideTits':
-            client.sadd(S_BOOBS_SET, imgId);
+            client.sadd(S_BOOBS_SET, imgId, errFunc);
+            client.zadd(S_BOOBS_SET, imgId, 1, errFunc);
             break;
         case 'frontalTits':
-            client.sadd(F_BOOBS_SET, imgId);
+            client.sadd(F_BOOBS_SET, imgId, errFunc);
+            client.zadd(F_BOOBS_SET, imgId, 1, errFunc);
             break;
         case 'None':
-            client.sadd(NP_SET, imgId);
+            client.sadd(NP_SET, imgId, errFunc);
+            client.zadd(NP_SET, imgId, 1, errFunc);
         }
 }
 
